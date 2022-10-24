aee3f30b4059b882575b67ca87a8f9d97c2b03ab
KNAndrikov18 <KNAndrikov18@codingburgas.bg>
2022-10-24 11:50:27 +0300

    Implementing class RecommenderImplementation and method recommendMovies

---
+++ src/main/java/com/example/zadacha/ZadachaApplication.java
@@ -0,0 +1,13 @@
+package com.example.zadacha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadachaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadachaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zadacha/ZadachaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zadacha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadachaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
